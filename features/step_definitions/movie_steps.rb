4ed4e729b613e9b64bda21244f9d4873b0de67c1
saasbook <saasbook@saasbook.(none)>
2012-10-27 13:55:40 +0100

    hw3 part 1, movie set up

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,13 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    new_movie = Movie.new(
+      :title => movie['title'],
+      :rating => movie['rating'],
+      :release_date => movie['release_date'],
+    );
+    new_movie.save!;
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
